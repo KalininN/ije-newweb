7d562028c82f954b82caa505cfa84123e48bd3d2
Nikolay Kalinin <nakalinin@gmail.com>
2017-01-05 21:43:48 +0300

    added scripts

---
+++ app.py
@@ -0,0 +1,152 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+from flask import session, redirect, url_for, escape, request
+from flask import render_template
+
+
+import ije
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        if "contest" not in session:
+            return redirect(url_for("choose_contest", error=u"Сначала необходимо выбрать контест"))
+        return render_template("login.html")
+    else:
+        login = request.form["login"]
+        password = request.form["password"]
+        if ije.is_login_valid(login, password):
+            session["login"] = login
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("login", error=u"Неправильный логин или пароль"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("login", None)
+    return redirect(url_for('index'))
+
+
+@app.route("/choose_contest", methods=["GET", "POST"])
+def choose_contest():
+    if request.method == "GET":
+        if "login" in session:
+            return redirect(url_for("index", error=u"Сначала необходимо выйти из системы"))
+        contests = ije.contests_list()
+        return render_template("choose_contest.html", contests=contests)
+    else:
+        contest_id = request.form["contest_id"]
+        if contest_id == "None":
+            session.pop("contest", None)
+            session.pop("contest_name", None)
+            return redirect(url_for("index"))
+        if ije.is_contest_valid(contest_id):
+            session["contest"] = contest_id
+            session["contest_name"] = ije.get_contest_name(contest_id)
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("choose_contest", error=u"Вы не можете войти в этот контест"))
+
+
+def get_uploaded_file(input_name):
+    if input_name not in request.files:
+        return None
+    f = request.files[input_name]
+    if not f or f.filename == "":
+        return None
+    return f
+
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    if not ije.can_submit():
+        return redirect(url_for("index", error=u"Вы не можете отправлять решения."))
+    if request.method == "GET":
+        return render_template("submit.html", problems=ije.problems_list(), languages=ije.languages_list())
+    else:
+        problem = request.form["problem"]
+        language = request.form["language"]
+        source_textarea = request.form["source_textarea"]
+        source_file = get_uploaded_file("source_file")
+        if source_file and source_textarea:
+            return redirect(url_for("submit", error=u"Вы не можете одновременно вставить исходный код и выбрать файл."))
+        elif not source_file and not source_textarea:
+            return redirect(url_for("submit", error=u"Выберите файл или скопируйте исходный код в форму."))
+        filename = os.path.join(ije.config['SUBMITS_FOLDER'], ije.get_filename(problem))
+        if source_file:
+            source_file.save(filename)
+        else:
+            with open(filename, "w") as f:
+                f.write(source_textarea)
+        return redirect(url_for("messages", message=u"Решение успешно отправлено! Удачи!"))
+
+
+@app.route("/messages")
+def messages():
+    if not ije.can_view_messages():
+        return redirect(url_for("index", error=u"Вы не можете просматривать сообщения."))
+    messages = ije.get_messages()
+    return render_template("messages.html", messages=messages)
+
+
+@app.route("/showmessage")
+def showmessage():
+    if not ije.can_view_messages():
+        return redirect(url_for("index", error=u"Вы не можете просматривать сообщения."))
+    messages = ije.get_messages()
+    message = None
+    message_id = request.args["id"]
+    for m in messages:
+        if str(m["id"]) == message_id:
+            message = m
+    if message is None:
+        return redirect(url_for("messages", error=u"Данное сообщение не найдено."))
+    return render_template("showmessage.html", message=message)
+
+
+@app.route("/monitor")
+def monitor():
+    if not ije.can_view_monitor():
+        return redirect(url_for("index", error=u"Вы не можете просматривать результаты."))
+    problems = ije.problems_list()
+    results = ije.get_results()
+    return render_template("monitor.html", problems=problems, results=results)
+
+
+def get_contest_time(smth):
+    return ije.get_contest_status_time()
+
+
+def format_time(mins):
+    if mins == "":
+        return ""
+    else:
+        return "%d:%02d" % (mins // 60, mins % 60)
+
+
+def shrink_comment(comment):
+    if len(comment) < 100:
+        return comment
+    pos = 100
+    if comment[pos:].find(";") < 5:
+        pos = pos + comment[pos:].find(";") + 1
+    return comment[:pos] + "..."
+
+
+app.jinja_env.filters['format_time'] = format_time
+app.jinja_env.filters['shrink_comment'] = shrink_comment
+app.jinja_env.filters['get_contest_time'] = get_contest_time
+
+
+# testing only. should be changed before using
+app.secret_key = b'\x800S\x9eM\x1c\xa6\xe4\x1b\x9a\xaf$\xa1/b\x1f9_A\xac*\xff\xa6\xcf'
---
+++ ije.py
@@ -0,0 +1,200 @@
+import os
+from operator import itemgetter, attrgetter
+
+
+from flask import session, redirect, url_for, escape, request
+
+
+import xml.etree.ElementTree as ET
+
+
+def parse_xml(filename):
+    tree = ET.parse(filename)
+    return tree.getroot()
+
+
+def get_contest_config():
+    if "contest" in session:
+        return parse_xml(session["contest"] + ".xml")
+    else:
+        return None
+
+def is_login_valid(login, password):
+    contest_config = get_contest_config()
+    if contest_config is None:
+        return False
+    for party_xml in contest_config.iter("party"):
+        if party_xml.attrib["id"] == login:
+            if party_xml.attrib["password"] == password:
+                return True
+            return False
+    return False
+
+
+def is_contest_valid(contest_id):
+    if "login" in session:
+        return False
+    contests = contests_list()
+    for contest in contests:
+        if contest["id"] == contest_id:
+            return True
+    return False
+
+
+def can_submit():
+    return "login" in session and "contest" in session
+
+
+def can_view_messages():
+    return "login" in session and "contest" in session
+
+
+to_short_verdict = {
+    "accepted": "AC",
+    "wrong-answer": "WA",
+    "presentation-error": "PE",
+    "time-limit-exceeded": "TL",
+    "idleness-limit-exceeded": "IL",
+    "memory-limit-exceeded": "ML",
+    "compilation-error": "CE",
+    "runtime-error": "RE",
+    "not-tested": "NT",
+    "fail": "FL",
+}
+
+
+def get_messages():
+    contest_xml = get_contest_config()
+    submits_xml = parse_xml(os.path.join(CONFIG_PATHS["results_dir"], contest_xml.attrib["submits"]))
+    submits = []
+    for submit_xml in submits_xml.iter("submit"):
+        if submit_xml.attrib["party"] == session["login"]:
+            comment = submit_xml.attrib["comment"]
+            comment = comment.replace("\n", "</br>")
+            comment = comment.replace(" ", "&nbsp;")
+            submits.append({"problem": submit_xml.attrib["problem"], "id": int(submit_xml.attrib["id"]),
+                            "time": int(submit_xml.attrib["time"]), "verdict": to_short_verdict[submit_xml.attrib["outcome"]],
+                            "comment": comment, "language": submit_xml.attrib["language-id"],
+                            "test": submit_xml.attrib["test"]})
+    submits = sorted(submits, key=itemgetter("time"), reverse=True)
+    return submits
+    #return [{"problem": "01.A", "id": "5", "time": 55, "verdict": "WA", "comment": "2nd nummbers differ"},
+            #{"problem": "01.B", "id": "5", "time": 65, "verdict": "AC", "comment": ""}]
+
+
+def can_view_monitor():
+    return "contest" in session
+
+
+def get_results():
+    contest_xml = get_contest_config()
+    results_xml = parse_xml(os.path.join(CONFIG_PATHS["results_dir"], contest_xml.attrib["monitor"]))
+    results = []
+    for team_xml in results_xml.iter("party"):
+        team = {"id": team_xml.attrib["id"],
+                "name": team_xml.attrib["name"],
+                "solved": int(team_xml.attrib["solved"]),
+                "penalty": int(team_xml.attrib["time"]),
+                "results": []
+                }
+        for problem_xml in team_xml.findall("problem"):
+            solved = int(problem_xml.attrib["solved"]) > 0
+            tries = abs((int(problem_xml.attrib["solved"])))
+            time = -1
+            if tries != 0:
+                time = problem_xml.findall("submit")[tries - 1].attrib["time"]
+            if time == -1:
+                team["results"].append({"verdict": "", "time": "", "style": "NO"})
+                continue
+            time = int(time)
+            if solved:
+                if tries == 1:
+                    team["results"].append({"verdict": "+", "time": time, "style": "AC"})
+                else:
+                    team["results"].append({"verdict": "+" + str(tries - 1), "time": time, "style": "AC"})
+            else:
+                team["results"].append({"verdict": "-" + str(tries), "time": time, "style": "RJ"})
+        results.append(team)
+    results = sorted(results, key=itemgetter("penalty"))
+    results = sorted(results, key=itemgetter("solved"), reverse=True)
+    if len(results) == 0:
+        return results
+    results[0]["place"] = 1
+    for i in range(1, len(results)):
+        if (results[i]["solved"], results[i]["penalty"]) == (results[i - 1]["solved"], results[i - 1]["penalty"]):
+            results[i]["place"] = results[i - 1]["place"]
+        else:
+            results[i]["place"] = i + 1
+    for team in results:
+        team["solved_parity"] = ["group_even", "group_odd"][team["solved"] % 2]
+    return results
+    #return [{"id": "NN1", "name": "Суперкоманда 1", "results": [
+                #{"verdict": "+", "time": "01:23"},
+                #{"verdict": "-4", "time": "02:23"}
+            #], "score": 1, "penalty": "83", "place": 1},
+            #{"id": "NN2", "name": "Суперкоманда 2", "results": [
+                #{"verdict": "+3", "time": "01:20"},
+                #{"verdict": ".", "time": ""}
+            #], "score": 1, "penalty": "140", "place": 2}]
+
+
+def contests_list():
+    contests_xml = parse_xml(CONFIG_PATHS["contests"])
+    contests = []
+    for contest_xml in contests_xml.iter("acm-contest"):
+        config_path = contest_xml.attrib["settings"]
+        config_xml = parse_xml(config_path)
+        contests.append({"id": config_path[:-4], "name": config_xml.attrib["title"], "config_path": config_path})
+    return contests
+
+
+def get_contest_name(id):
+    contests = contests_list()
+    for contest in contests:
+        if contest["id"] == id:
+            return contest["name"]
+    return None
+
+
+def get_contest_status_time():
+    contest_xml = get_contest_config()
+    results_xml = parse_xml(os.path.join(CONFIG_PATHS["results_dir"], contest_xml.attrib["monitor"]))
+    time = results_xml.attrib["time"]
+    status = results_xml.attrib["status"]
+    length = results_xml.attrib["length"]
+    return status + ", " + time + " из " + length
+
+
+def problems_list():
+    config_xml = get_contest_config()
+    problems = []
+    for problem_xml in config_xml.iter("problem"):
+        problems.append({"id": problem_xml.attrib["id"], "name": problem_xml.attrib["name"]})
+    return problems
+    #return [{"id": "01.A", "name": "problem A"}, {"id": "01.B", "name": "problem 2"}]
+
+
+def languages_list():
+    ije_xml = parse_xml(CONFIG_PATHS["ije"])
+    languages = []
+    for language_xml in ije_xml.iter("language"):
+        languages.append({"id": language_xml.attrib["id"], "name": language_xml.attrib["name"]})
+    return languages
+    #return [{"id": "cpp", "name": "C++"}, {"id": "pas", "name": "Pascal"}]
+
+
+def get_filename(problem):
+    return "submit.txt"
+
+
+config = {
+    "SUBMITS_FOLDER": "."
+}
+
+
+CONFIG_PATHS = {
+    "contests": "acm.xml",
+    "results_dir": "results",
+    "submits_dir": "sols_acm_reg",
+    "ije": "ije_cfg.xml"
+}
